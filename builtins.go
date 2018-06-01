d26ce6dad92ee70bac395ff9e3b5c814d8c1f5e8
Chong Guo <armourcy@gmail.com>
2018-06-01 00:12:31 -0700

    added builtins.go and test

---
+++ builtins.go
@@ -0,0 +1,82 @@
+package builtins
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver"
+)
+
+func getVersion(version string) ([]string, error) {
+	v, err := semver.NewVersion(version)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing version: %s", err)
+	}
+
+	coreModules := []string{
+		"assert",
+		"buffer",
+		"child_process",
+		"cluster",
+		"console",
+		"constants",
+		"crypto",
+		"dgram",
+		"dns",
+		"domain",
+		"events",
+		"fs",
+		"http",
+		"https",
+		"module",
+		"net",
+		"os",
+		"path",
+		"punycode",
+		"querystring",
+		"readline",
+		"repl",
+		"stream",
+		"string_decoder",
+		"sys",
+		"timers",
+		"tls",
+		"tty",
+		"url",
+		"util",
+		"vm",
+		"zlib",
+	}
+
+	v2, err := semver.NewVersion("6.0.0")
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing version: %s", err)
+	}
+	if v.LessThan(v2) {
+		coreModules = append(coreModules, "freelist")
+	}
+
+	newModules := []struct {
+		version     string
+		coreModules []string
+	}{
+		{"1.0.0", []string{"v8"}},
+		{"1.1.3", []string{"process"}},
+		{"8.1.0", []string{"async_hooks"}},
+		{"8.4.0", []string{"http2"}},
+		{"8.5.0", []string{"pref_hooks"}},
+	}
+
+	for _, n := range newModules {
+		v2, err := semver.NewVersion(n.version)
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing version: %s", err)
+		}
+		if v.GreaterThan(v2) || v.Equal(v2) {
+			for _, m := range n.coreModules {
+				coreModules = append(coreModules, m)
+			}
+		}
+	}
+
+	return coreModules, nil
+}
---
+++ builtins_test.go
@@ -0,0 +1,60 @@
+package builtins
+
+import (
+	"testing"
+)
+
+func TestSpecificModule(t *testing.T) {
+	tests := []struct {
+		version string
+		module  string
+		exist   bool
+	}{
+		{"5.99.99", "freelist", true},
+		{"6.0.0", "freelist", false},
+		{"0.99.99", "v8", false},
+		{"1.0.0", "v8", true},
+		{"1.0.99", "process", false},
+		{"1.1.0", "process", true},
+		{"8.0.99", "async_hooks", false},
+		{"8.1.0", "async_hooks", true},
+		{"8.3.99", "http2", false},
+		{"8.4.0", "http2", true},
+		{"8.4.99", "perf_hooks", false},
+		{"8.5.0", "perf_hooks", true},
+	}
+	for _, tc := range tests {
+		found := false
+		builtins, err := getVersion(tc.version)
+		if err != nil {
+			t.Error(err)
+		}
+		for _, b := range builtins {
+			if b == tc.module {
+				found = true
+			}
+		}
+		if found != tc.exist {
+			t.Errorf("Version check failed for module %s", tc.module)
+		}
+	}
+}
+
+func TestBuiltinsLength(t *testing.T) {
+	tests := []struct {
+		version string
+		length  int
+	}{
+		{"0.12.0", 33},
+		{"8.5.0", 37},
+	}
+	for _, tc := range tests {
+		builtins, err := getVersion(tc.version)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(builtins) != tc.length {
+			t.Errorf("Builtins length check failed for versoin %s", tc.version)
+		}
+	}
+}
